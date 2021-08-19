2172deb06cf34e7f7f8df92657be2259c2147c6c
Hasan <53163911+Zarrar12@users.noreply.github.com>
2021-08-19 17:58:28 +0500

    Update xdpBpf.c

--- xdpBpf.c
+++ xdpBpf.c
@@ -6,27 +6,59 @@
 #include <linux/in.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
+#include "libbpf/src/bpf.h"
+// #include "Torvalds_libbpf.h"
 
+#ifndef lock_xadd
+# define lock_xadd(ptr, val)              \
+        ((void)__sync_fetch_and_add(ptr, val))
+#endif
+
+#ifndef __inline
+# define __inline                         \
+        inline __attribute__((always_inline))
+#endif
+
+#ifndef BPF_FUNC
+# define BPF_FUNC(NAME, ...)              \
+        (*NAME)(__VA_ARGS__) = (void *)BPF_FUNC_##NAME
+#endif
 
 #define SEC(NAME) __attribute__((section(NAME), used))
-#define MY_IP	"192.168.2.101"
+#define MY_IP	0xC0A80265
+
+static void *BPF_FUNC(map_lookup_elem, void *map, const void *key);
+
+// struct bpf_elf_map cnt_map __section("maps") = {
+struct bpf_elf_map cnt_map = {
+	.type           = BPF_MAP_TYPE_ARRAY,
+	.size_key       = sizeof(uint32_t),
+	.size_value     = sizeof(uint32_t),
+	.pinning        = PIN_GLOBAL_NS,
+	.max_elem       = 1,
+};
 
 SEC("xdp")
 int dropper(struct xdp_md *ctx) {
 
+	int skbCnt = 11;
 	int mapFd;
-	int packs;
+	unsigned int *packs;
 	unsigned int dir = 0;
-	unsigned int ipsize = 0;
+	
 	void *data = (void *)(unsigned long)ctx->data;
 	void *data_end = (void *)(long)ctx->data_end;
 	
-	mapFd = bpf_obj_get("/sys/fs/bpf/ip/globals/cnt_map");
+	// mapFd = bpf_object__open("/sys/fs/bpf/ip/globals/cnt_map");
+	// mapFd = bpf_obj_get("/sys/fs/bpf/ip/globals/cnt_map");
+	// mapFd = bpf(BPF_OBJ_GET, &cnt_map);
+
+	mapFd = bpf_map__fd(&cnt_map);
 
 	struct ethhdr *eth = data;
-	ipsize = sizeof(struct ethhdr);
 	
-	struct iphdr *ip = eth + 1;	//Jump over the ethernet header
+	// struct iphdr *ip = eth + 1;	//Jump over the ethernet header
+	struct iphdr *ip = (struct iphdr*)((char*)eth + sizeof(struct ethhdr));	//Jump over the ethernet header
 
 	if ((data + sizeof(struct ethhdr) + sizeof(struct iphdr)) > data_end)
         return 0;
@@ -42,9 +74,10 @@ int dropper(struct xdp_md *ctx) {
 	if ((ip->ihl) < 5)
         return XDP_DROP;
 
-	
-	packs = bpf_map_lookup_elem(&mapFd, &dir);
-	if (packs == 20) {
+	// packs = &skbCnt;
+	packs = map_lookup_elem(&cnt_map, &dir);
+	if (*packs == skbCnt) {
+	if (mapFd)	
 		if ((ip->protocol == IPPROTO_UDP) && (ip->saddr == MY_IP))
 			return XDP_DROP;
 	}
